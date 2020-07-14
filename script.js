414ac3ef047353b8e243d990017c1efb97d626cb
Carlitos <juanes10ospina@hotmail.com>
2020-07-14 18:21:26 -0500

    src

--- script.js
+++
@@ -1,3 +0,0 @@
-prompt('Tail', 'Wind')
-if (true) {console.log('Tail')}
-setTimeout(() => {alert('Wind')},5000);
---
+++ src/script.js
@@ -0,0 +1,3 @@
+prompt('Tail', 'Wind')
+if (true) {console.log('Tail')}
+setTimeout(() => {alert('Wind')},5000);
